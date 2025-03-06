b87ee4ea113ddc323a239f81e09bb7f164d4765c
Hlib Bekysh <bekysh@3dvisionlabs.com>
2025-03-06 14:45:08 +0100

    Complete rework: instead of sem, use pthread and allow for multiple reader to read

--- exp1.cpp
+++ exp1.cpp
@@ -10,13 +10,30 @@
 
 const char* SHM_NAME = "/shm_mutex_example";
 const char* SEM_NAME = "/sem_mutex_example";
-const int SIZE = 5574;
+const int SIZE = 24*1024*1024;
 struct SharedData {
-    sem_t mutex;
+    pthread_cond_t reader_cv;
+    pthread_cond_t writer_cv;
+    pthread_mutex_t lock;
     size_t filesize;
-    char value[5574];
+    volatile int wlock;
+    volatile int num_of_reads;
+    char value[];
 };
-
+void signal_next(SharedData* shared)
+{
+    if (shared->wlock > 0)
+    {
+        // If any writes are waiting, wake one up
+        pthread_cond_signal(&shared->writer_cv);
+    }
+    else
+    {
+        // If there are no writes pending, wake up all the
+        // readers (there may not be any but that's fine)
+        pthread_cond_broadcast(&shared->reader_cv);
+    }
+}
 int main() {
     // Create shared memory
     int shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
@@ -24,14 +41,14 @@ int main() {
         perror("shm_open");
         return 1;
     }
-    std::ifstream file("testimage.png", std::ios::binary | std::ios::ate);
+    std::ifstream file("Nikon-D4-Shotkit-2.NEF", std::ios::binary | std::ios::ate);
     if (!file) {
         std::cerr << "Failed to open image.png\n";
         return 1;
     }
     // Set size of shared memory
     size_t fileSize = file.tellg();
-    std:: cout<<fileSize<<std::endl;
+    std:: cout<<fileSize/(1024*1024) <<" MB"<<std::endl;
     file.seekg(0, std::ios::beg);
     ftruncate(shm_fd, sizeof(SharedData) + SIZE);
 
@@ -44,32 +61,46 @@ int main() {
         return 1;
     }
     // Initialize semaphore (only needed once)
-    if (sem_init(&shared->mutex, 1, 1) == -1) {
-        perror("sem_init");
+    
+    if (pthread_mutex_init(&shared->lock, NULL) == -1) {
+        perror("pthread_mutex_init");
         return 1;
     }
     // Lock the semaphore before writing
-    int i=0;
-    while(true){
-        std::cout<< i<< std::endl;
-        i++;
-        sem_wait(&shared->mutex);
+    // while(true){
+        pthread_mutex_lock(&shared->lock);
         std::cout<< "MUTEX CLOSED"<< std::endl;
-        // Writing data
-        auto start = std::chrono::high_resolution_clock::now();
+        shared->wlock++;
+        std::cout<< shared->wlock<< std::endl;
+        std::cout<< "MUTEX 74"<< std::endl;
+        if (shared->wlock > 1 || shared->num_of_reads > 0){
+            pthread_cond_wait(&shared->writer_cv, &shared->lock);
+        }
+        std::cout<< "MUTEX 78"<< std::endl;
+        pthread_mutex_unlock(&shared->lock);
         shared->filesize = fileSize;
-        // Measure write time
+
+        std::cout<< "MUTEX 80"<< std::endl;
+        // Writing data
+                                    // Measure write time
+        auto start = std::chrono::high_resolution_clock::now();
         file.read(shared->value, fileSize);
         auto end = std::chrono::high_resolution_clock::now();
+        std::cout<< "MUTEX 86"<< std::endl;
+        pthread_mutex_lock(&shared->lock);
+        shared->wlock--;
+        signal_next(shared);
+        pthread_mutex_unlock(&shared->lock);
         double elapsed_time = std::chrono::duration<double, std::micro>(end - start).count();
         std::cout << "[Writer] Wrote value: " << "SIZE= "<<SIZE<< std::endl;
         std::cout << "[Writer] Wrote value: " << "SIZE= "<<shared->value<< std::endl;
-        std::cout << "Write Speed: " << (SIZE / (elapsed_time / 1e6)) / (1024 * 1024) << " MB/s" << std::endl;
+        std::cout << "Write Speed: " << (SIZE / (elapsed_time / 1e6)) / (1024 * 1024* 1024) << " MB/s" << std::endl;
         std::cout << "elapsed_time: " << elapsed_time << "ms" << std::endl;
-        sem_post(&shared->mutex); // Unlock
+        
+            // Unlock
         std::cout<< "MUTEX OPEN"<< std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-    }
+        std::this_thread::sleep_for(std::chrono::seconds(10));
+    // }
     // Cleanup
     munmap(shared, SIZE);
     close(shm_fd);
--- reader.cpp
+++ reader.cpp
@@ -9,14 +9,31 @@
 
 const char* SHM_NAME = "/shm_mutex_example";
 const char* SEM_NAME = "/sem_mutex_example";
-const int SIZE=5574;
+const int SIZE=25165824;
 
 struct SharedData {
-    sem_t mutex;
+    pthread_cond_t reader_cv;
+    pthread_cond_t writer_cv;
+    pthread_mutex_t lock;
     size_t filesize;
-    char value[5574];
+    volatile int wlock;
+    volatile int num_of_reads;
+    char value[];
 };
-
+void signal_next(SharedData* shared)
+{
+    if (shared->wlock > 0)
+    {
+        // If any writes are waiting, wake one up
+        pthread_cond_signal(&shared->writer_cv);
+    }
+    else
+    {
+        // If there are no writes pending, wake up all the
+        // readers (there may not be any but that's fine)
+        pthread_cond_broadcast(&shared->reader_cv);
+    }
+}
 int main() {
     // Open shared memory
     int shm_fd = shm_open(SHM_NAME, O_RDWR, 0666);
@@ -24,7 +41,7 @@ int main() {
         perror("shm_open");
         return 1;
     }
-    std::ifstream file("testimage.png", std::ios::binary | std::ios::ate);
+    std::ifstream file("Nikon-D4-Shotkit-2.NEF", std::ios::binary | std::ios::ate);
     if (!file) {
         std::cerr << "Failed to open image.png\n";
         return 1;
@@ -40,29 +57,41 @@ int main() {
     }
 
     // Lock the semaphore before reading
-    int i=0;
+    char* data = new char[shared->filesize];
+    std::ofstream output("output.NEF", std::ios::binary);
     while(true){
-        std::cout<< i<< std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(7000));
-        i++;
-        sem_wait(&shared->mutex);
-        std::cout<< "MUTEX CLOSED"<< std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-        size_t fileSize = shared->filesize;
-        auto start = std::chrono::high_resolution_clock::now();
-        std::ofstream output("output.png", std::ios::binary);
-        output.write(shared->value, fileSize);
-        auto end = std::chrono::high_resolution_clock::now();
-        std::cout << "[Reader] Read value: " << shared->value << std::endl;
-        std::cout << "[Reader] Read size: " << shared->filesize << std::endl;    
-        std::cout << "PNG file read from shared memory and saved as output.png\n";
-        double elapsed_time = std::chrono::duration<double, std::micro>(end - start).count();
-        std::cout << "Read Speed: " << (fileSize / (elapsed_time / 1e6)) / (1024 * 1024) << " MB/s" << std::endl;
-        std::cout << "elapsed_time: " << elapsed_time << "ms" << std::endl;
-        output.close();
-        sem_post(&shared->mutex); // Unlock
-        std::cout<< "MUTEX OPEN"<< std::endl;
+    while (shared->wlock > 0){
+        pthread_cond_wait(&shared->reader_cv, &shared->lock);
     }
+    shared->num_of_reads++;
+    std::cout<< shared->num_of_reads << std::endl;
+    std::cout<< "MUTEX CLOSED"<< std::endl;
+
+    pthread_mutex_unlock(&shared->lock);
+    std::cout<< "MUTEX OPEN"<< std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    auto start = std::chrono::high_resolution_clock::now();
+    data = shared->value;
+    auto end = std::chrono::high_resolution_clock::now();
+
+    pthread_mutex_lock(&shared->lock);
+    shared->num_of_reads--;
+    signal_next(shared);
+    pthread_mutex_unlock(&shared->lock);
+    size_t fileSize = shared->filesize;
+
+    std::cout << "[Reader] Read value: " << shared->value << std::endl;
+    std::cout << "[Reader] Read size: " << shared->filesize << std::endl;    
+    std::cout << "PNG file read from shared memory and saved as output.png\n";
+
+    double elapsed_time = std::chrono::duration<double, std::micro>(end - start).count();
+    std::cout << "Read Speed: " << (fileSize / (elapsed_time / 1e6)) / (1024 * 1024 * 1024) << " MB/s" << std::endl;
+    std::cout << "elapsed_time: " << elapsed_time << "ms" << std::endl;
+    
+    output.write(data, fileSize);
+    }
+    output.close();
+    
     // Cleanup
     munmap(shared, SIZE);
     close(shm_fd);
